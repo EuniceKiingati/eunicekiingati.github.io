ffdbee7c8751c3373fbf31844bfb0bbbdf438c8d
EuniceKiingati <47145776+EuniceKiingati@users.noreply.github.com>
2022-06-23 21:32:20 +0300

    added js email function

---
+++ contactform.js
@@ -0,0 +1,28 @@
+const sendEmail = ()=>{
+    const name=document.getElementById('name').value;
+    const email=document.getElementById('email').value;
+    const subject=document.getElementById('subject').value;
+    const message=document.getElementById('message').value;
+
+    if(name.trim() == '' || email.trim() =='' || subject.trim()=='' || message.trim()==''){
+        console.log("test")
+        return
+    }
+
+    var templateParams = {
+        from_name: name,
+        subject:subject,
+        message: message,
+        to_name: 'Eunice',
+        email: email,
+    };
+    emailjs.send('service_dmq8z78', 'template_ecdgia9', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+       document.getElementById("emailForm").reset();
+       alert("Sent")
+
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+}
